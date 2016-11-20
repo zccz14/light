16d17b54931efc2c36fafad788e345485c17481c
zccz14 <zccz14@outlook.com>
2016-11-20 17:31:46 +0800

    增加测试用例

--- test/user.js
+++ test/user.js
@@ -2,12 +2,58 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('User API Testing', function () {
-    it('retriving all the users and return json', function (done) {
+    it('retrieve return json', function (done) {
         request(app)
             .get('/user')
             .set('Accept', 'application/json')
             .expect(200)
             .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+    var newUser = {
+        username: 'zccz14',
+        password: '23336666'
+    };
+    it('create first user', function (done) {
+        request(app)
+            .post('/user')
+            .set('Accept', 'application/json')
+            .send({username: newUser.username, password: newUser.password})
+            .expect(200)
+            .expect({code: 0})
+            .expect((res) => {
+                if (res.body.body.length > 1) throw new Error('怎么还有人');
+                if (res.body.body[0].name != newUser.username) throw new Error('明明是我先来的');
+                newUser._id = res.body.body[0]._id;
+            })
+            .end(function(err, res) {
+                if (err) return done(err);
+                _id = res.body.
+                done();
+            });
+    });
+    it('update a exist user (change password)', function (done) {
+        request(app)
+            .put(`/user/${newUser._id}`)
+            .set('Accept', 'application/json')
+            .send({password: '23333333'})
+            .expect(200)
+            .expect({code: 0})
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+    it('update a non-exist user', function (done) {
+        request(app)
+            .put(`/user/heiheihei`)
+            .set('Accept', 'application/json')
+            .send({username: 'haha'})
+            .expect(200)
+            .expect({code: 1})
+            .end((err, res) => {
                 if (err) return done(err);
                 done();
             });
