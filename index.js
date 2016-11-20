8a6c5490b2b586ac0105467df96c7bd8567d5da3
zccz14 <zccz14@outlook.com>
2016-11-20 14:31:10 +0800

    add user route

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(session(configuration.system.session));
 // 注册路由
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/user', require('./routes/user'));
 
 // 监听端口
 app.listen(app.get('port'), () => {
