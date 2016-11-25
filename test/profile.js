fa77b18e79585f21c1db0543d8a864956406941b
aak1247 <aak1247@126.com>
2016-11-25 18:55:36 +0800

    test profile added

---
+++ test/profile.js
@@ -0,0 +1,74 @@
+const should = require('chai').should();
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../server');
+const db = require('../db');
+const config = require('../config');
+
+describe('profile', function () {
+    var aUser = {
+        email: 'zccz14@function-x.org',
+        password: 'world233'
+    }
+    var illegalEmail = 'zccz14';
+    var nonExistEmail = 'zccz1444@function-x.org';
+    var wrongPassword = 'world333';
+    var cookieid;
+    before('create a user before sign in', function (done) {
+        db.collection('users').createIndex({ email: 1 }, { unique: true }, function (err, reply) {
+            expect(err).to.be.null;
+            request(app)
+                .post('/user')
+                .set('Accept', 'application/json')
+                .send(aUser)
+                .expect(200)
+                .end(function (err, res) {
+                    expect(err).to.be.null;
+                    res.body.code.should.equal(0);
+                    done();
+                });
+        });
+
+    });
+    it('correct see profile', function (done) {
+        request(app)
+            .post('/user/sign-in')
+            .set('Accept', 'application/json')
+            .send(aUser)
+            .expect(200)
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                res.body.code.should.equal(0);
+                cookieid = res.headers['set-cookie'];
+                request(app)
+                    .get('/user/profile')
+                    .set('Accept', 'application/json')
+                    .set('cookie', cookieid)
+                    .expect(200)
+                    .end(function (err, res) {
+                        expect(err).to.be.null;
+                        res.body.code.should.equal(0);
+                        done();
+
+                    });
+            });
+    });
+
+    it('not signin', function (done) {
+        request(app)
+            .get('/user/profile')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                res.body.code.should.equal(23);
+                done();
+
+            });
+    });
+    after('drop users after tests', function (done) {
+        db.collection('users').drop(done);
+    });
+
+});
+
