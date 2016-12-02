8b5633c7bd117a0be5b3559121aa41dd30ce4b42
zccz14 <zccz14@outlook.com>
2016-12-02 15:09:43 +0800

    user role

--- models/user.js
+++ models/user.js
@@ -23,7 +23,10 @@ var UserSchema = new Schema({
             },
             message: 'password illegal'
         }
-    }
+    },
+    roles: [
+
+    ]
 });
 
 UserSchema.post('validate', function (user) {
