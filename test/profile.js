72dd3c455c956655f2efbd21572d50e07d02c15a
aak1247 <aak1247@126.com>
2016-12-02 14:47:48 +0800

    modify request method

--- test/profile.js
+++ test/profile.js
@@ -16,16 +16,16 @@ describe('profile', function () {
     var wrongPassword = 'world333';
     var cookieid;
     before('create a user before sign in', function (done) {
-        co(function* (){
-            var res1= yield request(app)
+        co(function* () {
+            var res1 = yield request(app)
                 .post('/user')
-                .set('Accept','application/json')
+                .set('Accept', 'application/json')
                 .send(aUser)
                 .expect(200);
-                res1.body.code.should.equal(0);
-                done();
+            res1.body.code.should.equal(0);
+            done();
         });
-        
+
     });
     it('correct see profile', function (done) {
         co(function* () {
@@ -47,13 +47,13 @@ describe('profile', function () {
     });
 
     it('not signin', function (done) {
-        co(function*(){
+        co(function* () {
             var res1 = yield request(app)
                 .get('/user/profile')
-                .set('Accept','application/json')
+                .set('Accept', 'application/json')
                 .expect(200);
-                res1.body.code.should.equal(7);
-                done();
+            res1.body.code.should.equal(7);
+            done();
         });
 
     });
--- test/signin.js
+++ test/signin.js
@@ -1,10 +1,11 @@
+const co = require('co');
 const should = require('chai').should();
 const expect = require('chai').expect;
 const request = require('supertest');
 const app = require('../server');
 const User = require('../models/user');
 
-describe('User Sign In', function() {
+describe('User Sign In', function () {
     var aUser = {
         email: 'zccz14@function-x.org',
         password: 'world233'
@@ -13,91 +14,87 @@ describe('User Sign In', function() {
     var nonExistEmail = 'zccz1444@function-x.org';
     var wrongPassword = 'world333';
 
-    before('create a user before sign in', function(done) {
-        request(app)
-            .post('/user')
-            .set('Accept', 'application/json')
-            .send(aUser)
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(0);
-                done();
-            });
+    before('create a user before sign in', function (done) {
+        co(function* () {
+            var res1 = yield request(app)
+                .post('/user')
+                .set('Accept', 'application/json')
+                .send(aUser)
+                .expect(200);
+            res1.body.code.should.equal(0);
+            done();
+        });
     });
-    it('correct sign in', function(done) {
-        request(app)
-            .post('/user/sign-in')
-            .set('Accept', 'application/json')
-            .send(aUser)
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(0);
-                done();
-            });
+    it('correct sign in', function (done) {
+        co(function* () {
+            var res1 = yield request(app)
+                .post('/user/sign-in')
+                .set('Accept', 'application/json')
+                .send(aUser)
+                .expect(200);
+            res1.body.code.should.equal(0);
+            done();
+        });
     });
-    it('a nonexist user', function(done) {
-        request(app)
-            .post('/user/sign-in')
-            .set('Accept', 'application/json')
-            .send({ email: nonExistEmail, password: aUser.password })
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(5);
-                done();
-            });
+    it('a nonexist user', function (done) {
+        co(function* () {
+            var res1 = yield request(app)
+                .post('/user/sign-in')
+                .set('Accept', 'application/json')
+                .send({ email: nonExistEmail, password: aUser.password })
+                .expect(200);
+            res1.body.code.should.equal(5);
+            done();
+        });
     });
-    it('wrong password', function(done) {
-        request(app)
-            .post('/user/sign-in')
-            .set('Accept', 'application/json')
-            .send({ email: aUser.email, password: wrongPassword })
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(5);
-                done();
-            });
+    it('wrong password', function (done) {
+        co(function* () {
+            var res1 = yield request(app)
+                .post('/user/sign-in')
+                .set('Accept', 'application/json')
+                .send({ email: aUser.email, password: wrongPassword })
+                .expect(200);
+            res1.body.code.should.equal(5);
+            done();
+        });
     });
-    it('empty email', function(done) {
-        request(app)
-            .post('/user/sign-in')
-            .set('Accept', 'application/json')
-            .send({ password: wrongPassword })
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(5);
-                done();
-            });
+    it('empty email', function (done) {
+        co(function* () {
+            var res1 = yield request(app)
+                .post('/user/sign-in')
+                .set('Accept', 'application/json')
+                .send({ password: wrongPassword })
+                .expect(200);
+            res1.body.code.should.equal(5);
+            done();
+        });
+        
     });
-    it('empty password', function(done) {
+    it('empty password', function (done) {
         request(app)
             .post('/user/sign-in')
             .set('Accept', 'application/json')
             .send({ email: aUser.email })
             .expect(200)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(err).to.be.null;
                 res.body.code.should.equal(5);
                 done();
             });
     });
-    it('illegal email', function(done) {
+    it('illegal email', function (done) {
         request(app)
             .post('/user/sign-in')
             .set('Accept', 'application/json')
             .send({ email: illegalEmail, password: aUser.password })
             .expect(200)
-            .end(function(err, res) {
+            .end(function (err, res) {
                 expect(err).to.be.null;
                 res.body.code.should.equal(5);
                 done();
             });
     });
-    after('drop users after tests', function(done) {
+    after('drop users after tests', function (done) {
         User.remove({}, done);
     });
 });
