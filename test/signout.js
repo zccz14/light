4fde4fde13ff214f04ad992c6adc8537e94bcdf3
ddhee <kidakitaki@gmail.com>
2016-12-02 14:12:58 +0800

    promise reconstruct

--- test/signout.js
+++ test/signout.js
@@ -13,21 +13,18 @@ describe('User Sign Out', function () {
     }
     var cookie;
     var noCookie = '';
-
     before('create a user', function (done) {
-        request(app)
-            .post('/user')
-            .set('Accept', 'application/json')
-            .send(aUser)
-            .expect(200)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(0);
-                cookie = res.headers['set-cookie'];
-                done();
-            });
+        co(function* (){
+            var res = yield request(app)
+                .post('/user')
+                .set('Accept', 'application/json')
+                .send(aUser)
+                .expect(200)
+            res.body.code.should.equal(0);
+            cookie = res.headers['set-cookie'];
+            done();
+        });
     });
-
     it('correct sign-out', function (done) {
         co(function* () {
             var res1 = yield request(app)
@@ -45,30 +42,28 @@ describe('User Sign Out', function () {
         });
     });
     it('just signed up but have not signed in yet', function (done) {
-        request(app)
-            .get('/user/sign-out')
-            .set('Accept', 'application/json')
-            .set('Cookie', cookie)
-            .expect(200)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(0);
-                done();
-            });
+        co(function* (){
+            var res = yield request(app)
+                .get('/user/sign-out')
+                .set('Accept', 'application/json')
+                .set('Cookie', cookie)
+                .expect(200)
+            res.body.code.should.equal(0);
+            done();
+        });
     });
     it('have not signed in or signed up yet', function (done) {
-        request(app)
-            .get('/user/sign-out')
-            .set('Accept', 'application/json')
-            .set('Cookie', noCookie)
-            .expect(200)
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                res.body.code.should.equal(0);
-                done();
-            });
+        co(function* (){
+            var res = yield request(app)
+                .get('/user/sign-out')
+                .set('Accept', 'application/json')
+                .set('Cookie', noCookie)
+                .expect(200)
+            res.body.code.should.equal(0);
+            done();
+        });
     });
-    after(function (done) {
+    after('drop user after tests', function (done) {
         User.remove({}, done);
     });
 });
