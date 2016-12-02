fbd8cf4aaa918e45705485b54028938c6ed126bd
zccz14 <zccz14@outlook.com>
2016-12-02 20:45:09 +0800

    @FIXED: User Handler promise rewrite

--- routes/on_error.js
+++ routes/on_error.js
@@ -11,6 +11,7 @@ module.exports = function(res) {
                 errors: err.errmsg
             });
         } else {
+            console.log(err);
             res.json({
                 code: 1
             });
