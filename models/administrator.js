65dc02cfe7ae5d16ea576027d669e35a0794e529
zccz14 <zccz14@outlook.com>
2016-12-17 11:28:58 +0800

    [model] administrator

---
+++ models/administrator.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AdministratorSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    }
+});
+
+const Administrator = mongoose.model('administrator', AdministratorSchema);
+module.exports = Administrator;
