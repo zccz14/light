23b72f9cd987d5ef3e4b3797e0dec4169f23302a
zccz14 <zccz14@outlook.com>
2016-12-17 17:21:06 +0800

    [API] Get Available Problem List

--- routes/problem_list.js
+++ routes/problem_list.js
@@ -45,6 +45,32 @@ module.exports = express.Router()
       });
     }).catch(OnError(res));
   })
+  // find available problemlist
+  .get('/', function(req, res, next){
+    co(function*(){
+      // all public problem lists with query
+      let $query = Object.assign({
+        $or: [
+          {public: true}
+        ]
+      }, req.query);
+      // if user has signed in
+      if (req.session.user) {
+        let user = req.session.user;
+        // find problem lists of user's groups
+        let ownerIds = user.roles.map(v => new ObjectId(v.group));
+        // find user's problem lists
+        ownerIds.push(new ObjectId(user._id));
+        $query.$or.push({
+          ownerId: {
+            $in: ownerIds
+          }
+        });
+      }
+      let problemLists = yield ProblemList.find($query).exec();
+      res.json({code: 0, problemLists});
+    }).catch(OnError(res));
+  })
 
   //find a problemlist
   .get('/:_id', function (req, res, next) {
