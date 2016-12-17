f8cdec8bde9e3d1a5e4f872342f04a1d707f2870
zccz14 <zccz14@outlook.com>
2016-12-17 11:49:02 +0800

    [API] add an administrator

---
+++ routes/system.js
@@ -0,0 +1,29 @@
+const co = require('co');
+const express = require('express');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
+const User = require('../models/user');
+const Administrator = require('../models/administrator');
+const OnError = require('./on_error');
+const AccessControl = require('./access_control');
+
+module.exports = express.Router()
+    // Let a user to be system administrator
+    .post('/administrator', AccessControl.signIn)
+    .post('/administrator', function (req, res, next) {
+        co(function* () {
+            let user = req.session.user;
+            let userId = req.body.userId || user._id;
+            if (!ObjectId.isValid(userId)) {
+                res.json({ code: 2 });
+                return;
+            }
+            let admins = yield Administrator.find({}).exec();
+            let newAdmin = new Administrator({
+                userId: new ObjectId(userId)
+            });
+            newAdmin = yield newAdmin.save();
+            res.json({ code: 0 });
+        }).catch(OnError(res));
+    })
