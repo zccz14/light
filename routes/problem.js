d1be5dfc34be8660f7a9a30faa7be98ddea44585
zccz14 <zccz14@outlook.com>
2016-12-19 20:46:12 +0800

    重构 Problem API

--- routes/problem.js
+++ routes/problem.js
@@ -10,44 +10,10 @@ module.exports = express.Router()
     // Admin API
     .use('/', AccessControl.admin)
     // create a problem
-    .post('/', function(req, res, next) {
-        co(function* () {
-            let newProblem = new Problem({
-                title: (req.body.title || '').trim(),
-                description: (req.body.description || '').trim(),
-            });
-            newProblem = yield newProblem.save();
-            res.json({ code: 0 });
-        }).catch(OnError(res));
-    })
+    .post('/', require('../lib/problem_create'))
     // get problem list
-    .get('/', function(req, res, next) {
-        co(function* () {
-            let problems = yield Problem.find(req.query).exec();
-            res.json({
-                code: 0,
-                problems
-            });
-        }).catch(OnError(res));
-    })
+    .get('/', require('../lib/problem_retrieve'))
     // update a problem
-    .put('/:_id', function(req, res, next) {
-        co(function* () {
-            let problem = yield Problem.update(
-                {
-                    _id: new ObjectId(req.params._id)
-                },
-                {
-                    $set: req.body
-                }
-            ).exec();
-            res.json({ code: 0 });
-        }).catch(OnError(res));
-    })
+    .put('/:_id', require('../lib/problem_update'))
     // delete a problem
-    .delete('/:_id', function(req, res, next) {
-        co(function* () {
-            let problem = yield Problem.findByIdAndRemove(req.params._id).exec();
-            res.json({ code: 0 });
-        }).catch(OnError(res));
-    })
+    .delete('/:_id', require('../lib/problem_delete'))
