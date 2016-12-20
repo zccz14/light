a526827150917b67ee0cf049f1b62924dfc00991
sylxjtu <sylxjtu@users.noreply.github.com>
2016-12-20 20:19:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.use(cookieParser());
 server.use(session(configuration.system.session));
 
 server.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080/');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
