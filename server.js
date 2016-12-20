734cdb7590b7994d9257d507aa0f36ae02744b90
sylxjtu <sylxjtu@users.noreply.github.com>
2016-12-20 20:23:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ server.use(session(configuration.system.session));
 server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
 
     next();
 })
