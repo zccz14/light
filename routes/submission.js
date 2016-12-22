0724be89cfd6eb8ba200d719aeb8d7bc4048347e
zccz14 <zccz14@outlook.com>
2016-12-23 07:11:31 +0800

    哇哦 闭环完成

--- routes/submission.js
+++ routes/submission.js
@@ -1,4 +1,7 @@
-module.exports = require('express')
-                     .Router()
-                     .post('/', require('../lib/submission_create'))
-                     .get('/', require('../lib/submission_retrieve'))
+module.exports =
+    require('express')
+        .Router()
+        .post('/', require('../lib/submission_create'))
+        .get('/', require('../lib/submission_retrieve'))
+        .get('/:submissionId', require('../lib/submission_detail'))
+        .put('/:submissionId', require('../lib/submission_sentence_update'))
