ef43dfc102ce63082794e58e4638b96cffef2ff9
zccz14 <zccz14@outlook.com>
2016-12-28 02:54:14 +0800

    JSDOC Update

--- schemas/problem.js
+++ schemas/problem.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+/**
+ * @class Problem
+ */
 var ProblemSchema = new Schema({
+  /**
+   * 题目的所有者 ID
+   * @memberof Problem~
+   * @type {ObjectId}
+   */
   ownerId: Schema.Types.ObjectId,
-  title: {type: String, required: true},
-  description: {type: String, required: true}
+  /**
+   * 题目的标题
+   * @memberof Problem~
+   * @type {String}
+   */
+  title: {
+    type: String,
+    required: true
+  },
+  /**
+   * 题目描述
+   * @memberof Problem~
+   * @type {String}
+   */
+  description: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = ProblemSchema;
