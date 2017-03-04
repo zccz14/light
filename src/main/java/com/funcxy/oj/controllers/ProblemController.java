d32f2cb8e014ac3f40366c4ca3a8ba1a40f7f737
wtupc96 <594373559@qq.com>
2017-03-04 14:38:55 +0800

    Use stream to implement PUT.

--- src/main/java/com/funcxy/oj/controllers/ProblemController.java
+++ src/main/java/com/funcxy/oj/controllers/ProblemController.java
@@ -5,12 +5,16 @@ import com.funcxy.oj.repositories.ProblemRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by wtupc96 on 2017/2/28.
@@ -26,15 +30,13 @@ public class ProblemController {
     MongoTemplate mongoTemplate;
 
     @RequestMapping(method = RequestMethod.POST)
-    public Problem saveProblem(@Valid Problem problem, HttpSession session) {
-        ObjectId userId = (ObjectId) session.getAttribute("userId");
-        if(!ObjectId.isValid(userId.toHexString()))
-            return problemRepository.save(problem);
+    public ResponseEntity<Problem> saveProblem(@Valid Problem problem, HttpSession session) {
+        return new ResponseEntity<>(problemRepository.save(problem), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Problem getOneSpecificProblem(@PathVariable ObjectId id) {
-        return problemRepository.findById(id);
+    public ResponseEntity<Problem> getOneSpecificProblem(@PathVariable ObjectId id, HttpSession session) {
+        return new ResponseEntity<Problem>(problemRepository.findById(id), HttpStatus.OK);
     }
 
 //    @RequestMapping(method = RequestMethod.GET)
@@ -56,7 +58,7 @@ public class ProblemController {
 //    }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Problem> getProblem(Problem problem) {
+    public ResponseEntity<List<Object>> getProblem(Problem problem, HttpSession session) {
         List<Problem> problemIdList = new ArrayList<>();
         if (problem.getType() != null) {
             problemIdList.addAll(problemRepository.findByTheArg("type", problem.getType()));
@@ -77,23 +79,40 @@ public class ProblemController {
             }
         }
 
-        return problemIdList;
+        class cleanedProblem implements Serializable {
+            public ObjectId id;
+            public String title;
+
+            public cleanedProblem(ObjectId id, String title) {
+                this.id = id;
+                this.title = title;
+            }
+        }
+
+
+        return
+                new ResponseEntity<List<Object>>
+                        (problemIdList
+                                .stream()
+                                .map(pro ->
+                                        new cleanedProblem(pro.getId(), pro.getTitle()))
+                                .collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Problem updateProblem(@RequestBody @Valid Problem problem, @PathVariable ObjectId id) {
+    public ResponseEntity<Problem> updateProblem(@RequestBody @Valid Problem problem, @PathVariable ObjectId id, HttpSession session) {
         Problem tempProblem = problemRepository.findById(id);
         if (problem.getReferenceAnswer() != null) {
             problem.setReferenceAnswer(tempProblem.getReferenceAnswer());
         }
         problem.setId(id);
-        return problemRepository.save(problem);
+        return new ResponseEntity<Problem>(problemRepository.save(problem), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public Problem deleteProblem(@PathVariable ObjectId id) {
+    public ResponseEntity<Problem> deleteProblem(@PathVariable ObjectId id, HttpSession session) {
         Problem tempProblem = problemRepository.findById(id);
         problemRepository.delete(tempProblem);
-        return tempProblem;
+        return new ResponseEntity<Problem>(tempProblem, HttpStatus.OK);
     }
 }
