ccef3b4a9f4c22c52c7404145aaf5f7eefb8d29c
wtupc96 <594373559@qq.com>
2017-03-04 12:03:27 +0800

    Controller GET menthod returns id and title.

--- src/main/java/com/funcxy/oj/repositories/ProblemRepository.java
+++ src/main/java/com/funcxy/oj/repositories/ProblemRepository.java
@@ -4,6 +4,7 @@ import com.funcxy.oj.models.Problem;
 import com.funcxy.oj.models.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
@@ -15,4 +16,7 @@ public interface ProblemRepository extends MongoRepository<Problem, ObjectId> {
     public List<Problem> findByCreator(User creator);
     public List<Problem> findByTitle(String title);
     public List<Problem> findByType(String type);
+
+    @Query(value = "{?0:?1}", fields = "{'title':1}")
+    public List<Problem> findByTheArg(String arg, Object argValue);
 }
