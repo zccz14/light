fa5b2ee8b80150ea0847141f9ecb1cac6e408ff0
zccz14 <zccz14@outlook.com>
2017-03-10 16:04:34 +0800

    Cors Filter Update Session Controller

--- src/main/java/com/funcxy/oj/CorsFilter.java
+++ src/main/java/com/funcxy/oj/CorsFilter.java
@@ -21,10 +21,11 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(servletRequest, response);
     }
 
