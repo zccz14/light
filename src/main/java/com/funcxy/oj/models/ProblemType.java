d4a351b108d3f26ced5d1bab2f3dd40f8a6a827a
zccz14 <zccz14@outlook.com>
2017-03-12 11:54:52 +0800

    Simplify SignInPassport Add ProblemType enum

---
+++ src/main/java/com/funcxy/oj/models/ProblemType.java
@@ -0,0 +1,19 @@
+package com.funcxy.oj.models;
+
+/**
+ * 题目类型枚举
+ */
+public enum ProblemType {
+    /**
+     * 单项选择题
+     */
+    SINGLE_CHOICE,
+    /**
+     * 判断题
+     */
+    TRUE_OR_FALSE,
+    /**
+     * 开放题
+     */
+    OPEN_QUESTION
+}
