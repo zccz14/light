891e64bbe86d05f93458e3ebd1aff11fe4e29c00
wtupc96 <594373559@qq.com>
2017-03-13 14:47:29 +0800

    Good.

--- src/main/java/com/funcxy/oj/controllers/SubmissionController.java
+++ src/main/java/com/funcxy/oj/controllers/SubmissionController.java
@@ -8,12 +8,7 @@ import com.funcxy.oj.repositories.ProblemRepository;
 import com.funcxy.oj.repositories.SubmissionRepository;
 import com.funcxy.oj.repositories.UserRepository;
 import com.funcxy.oj.utils.UserUtil;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +16,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.funcxy.oj.utils.UserUtil.isSignedIn;
 
 /**
- * @author  niyou2016 on 2017/3/2 0002.
+ * @author niyou2016 on 2017/3/2 0002.
  */
 @RestController
 @RequestMapping("/submission")
@@ -38,7 +32,7 @@ public class SubmissionController {
     private final ProblemRepository problemRepository;
 
     @Autowired
-    public SubmissionController(SubmissionRepository submissionRepository,  ProblemListRepository problemListRepository, UserRepository userRepository,ProblemRepository problemRepository) {
+    public SubmissionController(SubmissionRepository submissionRepository, ProblemListRepository problemListRepository, UserRepository userRepository, ProblemRepository problemRepository) {
         this.submissionRepository = submissionRepository;
         this.problemListRepository = problemListRepository;
         this.userRepository = userRepository;
@@ -47,7 +41,8 @@ public class SubmissionController {
 
     /**
      * 查找提交
-     * @param id 提交Id
+     *
+     * @param id      提交Id
      * @param session session
      * @return 成功时返回提交对象
      */
@@ -57,16 +52,17 @@ public class SubmissionController {
             return new ResponseEntity<>(new ForbiddenError(), HttpStatus.FORBIDDEN);
         }
         Submission submission = submissionRepository.findById(id);
-        if (submission == null){
-            return new ResponseEntity<>(new NotFoundError(),HttpStatus.NOT_FOUND);
+        if (submission == null) {
+            return new ResponseEntity<>(new NotFoundError(), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(submission, HttpStatus.OK);
     }
 
     /**
      * 创建提交
+     *
      * @param submission 提交对象
-     * @param session session对象
+     * @param session    session对象
      * @return 成功时返回提交对象
      */
     @RequestMapping(method = RequestMethod.POST)
@@ -78,16 +74,16 @@ public class SubmissionController {
 
         //TODO:鉴权
         User user = userRepository.findById(session.getAttribute("userId").toString());
-        ProblemList problemList = problemListRepository .findById(submission.getProblemListId());
+        ProblemList problemList = problemListRepository.findById(submission.getProblemListId());
         Problem problem = problemRepository.findById(submission.getProblemId());
 
-        if (user == null|| problemList == null || problem == null) {
-            return new ResponseEntity<>(new NotFoundError(),HttpStatus.NOT_FOUND);
+        if (user == null || problemList == null || problem == null) {
+            return new ResponseEntity<>(new NotFoundError(), HttpStatus.NOT_FOUND);
         }
         User judger = userRepository.findById(problemList.getJudgerList().get(problemList.getProblemIds().indexOf(problem.getId())));
 
-        if (judger == null){
-            return new ResponseEntity<>(new NotFoundError(),HttpStatus.NOT_FOUND);
+        if (judger == null) {
+            return new ResponseEntity<>(new NotFoundError(), HttpStatus.NOT_FOUND);
         }
 
         user.addSubmissionHistory(submission.getId());
@@ -96,19 +92,20 @@ public class SubmissionController {
         //加入Message
         //TODO:考虑读取配置文件，或保存配置名称
         judger.addMessage(new Message("Submission to judge",
-                "A submission by "+user.getUsername()+" to promblem"+
-                        problem.getTitle()+
+                "A submission by " + user.getUsername() + " to promblem" +
+                        problem.getTitle() +
                         " has just been created , don't forget to decide in time.",
-                        MessageType.TOJUDGE));
+                MessageType.TOJUDGE, submission.getId()));
         userRepository.save(judger);
         return new ResponseEntity<>(submissionRepository.insert(submission), HttpStatus.OK);
     }
 
     /**
      * 修改判决/处理提交
-     * @param submission 提交对象（包含判决）
+     *
+     * @param submission   提交对象（包含判决）
      * @param submissionId 提交Id
-     * @param session session对象
+     * @param session      session对象
      * @return 成功时返回新提交
      * 已判决的题目不可再标记为未判决
      */
@@ -117,8 +114,8 @@ public class SubmissionController {
                                          @PathVariable String submissionId,
                                          HttpSession session) {
         // UserId
-        if (!UserUtil.isSignedIn(session)){
-            return new ResponseEntity<>(new ForbiddenError(),HttpStatus.FORBIDDEN);
+        if (!UserUtil.isSignedIn(session)) {
+            return new ResponseEntity<>(new ForbiddenError(), HttpStatus.FORBIDDEN);
         }
         User user = userRepository.findById(session.getAttribute("userId").toString());
         Submission theSubmission = submissionRepository.findById(submissionId);
@@ -130,8 +127,8 @@ public class SubmissionController {
         if (judger == null) {
             return new ResponseEntity<>(new NotFoundError(), HttpStatus.NOT_FOUND);
         }
-        if (!judger.getId().equals(user.getId())){
-            return new ResponseEntity<>(new ForbiddenError(),HttpStatus.FORBIDDEN);
+        if (!judger.getId().equals(user.getId())) {
+            return new ResponseEntity<>(new ForbiddenError(), HttpStatus.FORBIDDEN);
         }
         if (judger.getId().equals(user.getId())) {
             theSubmission.setSentence(submission.getSentence());
@@ -140,10 +137,10 @@ public class SubmissionController {
             submitter.addMessage(
                     new Message(
                             "Sentence updated",
-                            "Your submission to the problem "+
-                                problemRepository.findById(theSubmission.getProblemId()).getTitle()+
+                            "Your submission to the problem " +
+                                    problemRepository.findById(theSubmission.getProblemId()).getTitle() +
                                     "has just been sentenced. Don't forget to take a look.",
-                            MessageType.SENTENCE
+                            MessageType.SENTENCE, submission.getId()
                     )
             );
             userRepository.save(submitter);
