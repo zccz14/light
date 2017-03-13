0a90d51ceb27c31ecec9dd9875ba24df52a39f47
aak1247 <aak1247@126.com>
2017-03-13 15:21:12 +0800

    Merge

--- src/main/java/com/funcxy/oj/controllers/SubmissionController.java
+++ src/main/java/com/funcxy/oj/controllers/SubmissionController.java
@@ -24,7 +24,7 @@ import static com.funcxy.oj.utils.UserUtil.isSignedIn;
  * @author niyou2016 on 2017/3/2 0002.
  */
 @RestController
-@RequestMapping("/submission")
+@RequestMapping("/submissions")
 
 public class SubmissionController {
     private final SubmissionRepository submissionRepository;
