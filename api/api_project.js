7e71098c84aca15e59658bc40d95b376974f6578
zccz14 <zccz14@outlook.com>
2017-06-29 19:15:15 +0000

    Travis Deploy

---
+++ api/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Orange Juice API",
+  "version": "0.1.0",
+  "description": "Orange Juice Full API Documentation",
+  "title": "Orange Juice API",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-06-29T19:15:15.327Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
